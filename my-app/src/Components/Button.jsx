b986b6d672e88e5d3ac22ad8cf5222dbae2a7027
gaurav78528 <gauravsuthar309@gmail.com>
2022-09-28 19:00:48 +0530

    added day-2 work

---
+++ my-app/src/Components/Button.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+const CurrencyButton = ({ text, onOpen, isIcon }) => {
+  return isIcon ? (
+    <img
+      src="https://cf.bstatic.com/static/img/flags/new/48-squared/in/20aa535a5d3c505dd02fea275ed1a36c0fb1fe08.png"
+      alt="img"
+      onClick={onOpen}
+      style={{
+        borderRadius: "50%",
+        width: "30px",
+        marginRight: "10px",
+        cursor: "pointer",
+      }}
+    />
+  ) : (
+    <Button onClick={onOpen} colorScheme="#003580">
+      {text}
+    </Button>
+  );
+};
+
+export default CurrencyButton;
